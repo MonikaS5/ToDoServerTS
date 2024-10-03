e1a2c7d5c744cd7373c2d84e071b4b40bc70366a
MonikaS5 <monika.bhagade@gmail.com>
2024-10-03 17:16:17 +0100

    update2

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ const TodoModel = require("./models/todoList")
 
 dotenv.config();
 var app = express();
-app.use(cors({
-    origin:'https://lucent-kelpie-112227.netlify.app/',
-}
-    
-));
+app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
